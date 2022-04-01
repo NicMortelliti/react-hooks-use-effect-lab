630c518ba89891818b962f5b8bec10bce34d9e92
NicMortelliti <npmortelliti@gmail.com>
2022-04-01 16:31:19 -0700

    Add useEffect to Question component. Assignment not completed, yet

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    setTimeout(
+      () =>
+        timeRemaining !== 0 ? setTimeRemaining(timeRemaining - 1) : timesUp,
+      1000
+    );
+  }, [timeRemaining]);
+
+  function timesUp() {
+    setTimeRemaining(10);
+    onAnswered(false);
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
